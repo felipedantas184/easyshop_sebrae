38a51df7de3da2d5543e9c8d8405f24cd0270364
Felipe Augusto Oliveira Dantas <felipedantas184@gmail.com>
2024-12-20 11:43:36 -0300

    app analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { AuthContextProvider } from "@/context/AuthContext";
 import ProtectedRoute from "@/context/ProtectedRoutes";
+import Script from "next/script";
 
 function useRouterReady() {
   const [isReady, setIsReady] = useState(false);
@@ -28,11 +29,31 @@ export default function App({ Component, pageProps }: AppProps) {
     <AuthContextProvider>
       {!AuthRequired.includes(router.pathname) ? (
         <Provider store={store} >
+          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-TJQGSGGCG4"></Script>
+          <Script id='google-analytics' >
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-TJQGSGGCG4');
+            `}
+          </Script>
           <Component {...pageProps} />
         </Provider>
       ) : (
         <Provider store={store} >
           <ProtectedRoute>
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-MWK8WVG5KZ"></Script>
+            <Script id='google-analytics' >
+              {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-TJQGSGGCG4');
+            `}
+            </Script>
             <Component {...pageProps} />
           </ProtectedRoute>
         </Provider>
